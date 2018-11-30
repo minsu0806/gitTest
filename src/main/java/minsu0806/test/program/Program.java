03973eb878de2030bb15dcc1dcde98b509f5a058
msjang <minsu0806@gmail.com>
2018-12-01 01:37:51 +0900

    create Program

---
+++ src/main/java/minsu0806/test/program/Program.java
@@ -0,0 +1,10 @@
+package minsu0806.test.program;
+
+public class Program {
+	public void Worker() {
+		System.out.println(getLogic());
+	}
+	public String getLogic() {
+		return "master logic";
+	}
+}
